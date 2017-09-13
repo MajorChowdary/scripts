b8507c94a088e48ba7d3e5e681eb11d31e3a8ae6
Raghu K <raghuk.vit@gmail.com>
2017-09-13 11:49:11 +0000

    Updating scripts

---
+++ common-functions.sh
@@ -0,0 +1,23 @@
+### Colors
+R="\e[31m"
+B="\e[34m"
+Y="\e[33m"
+N="\e[0m"
+
+## Common Functions
+
+### Print Functions
+info() {}
+success() {}
+error() {
+	echo -e "$R$1$N"
+}
+
+### Checking Root User or not
+CheckRoot() {
+LID=$(id -u)
+if [ $LID -ne 0 ]; then 
+	error "Your must be a root user to perform this command.."
+	exit 1
+fi
+}
