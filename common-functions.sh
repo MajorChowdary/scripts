52d485a0b8fd7d5d3dd464a802bd5c0ce9d1f59b
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-09-14 12:45:20 +0530

    Updating scripts

--- common-functions.sh
+++ common-functions.sh
@@ -2,18 +2,23 @@
 R="\e[31m"
 B="\e[34m"
 Y="\e[33m"
+G="\e[32m"
 N="\e[0m"
+
+ELV=$(rpm -q basesystem |sed -e 's/\./ /g' |xargs -n 1|grep ^el)
 
 ## Common Functions
 
 ### Print Functions
 hint() {
-	echo -e "➜ Hint: $1$N"
+	echo -e "➜  Hint: $1$N"
 }
 info() {
 	echo -e " $1$N"
 }
-#success() {:;}
+success() {
+	echo -e "${G}✓  $1$N"
+}
 error() {
 	echo -e "${R}✗  $1$N"
 }
@@ -32,8 +37,22 @@ CheckSELinux() {
 	STATUS=$(sestatusx | grep 'SELinux status:'| awk '{print $NF}')
 	if [ "$STATUS" != 'disabled' ]; then 
 		error "SELINUX Enabled on the server, Hence cannot proceed. Please Disable it and run again.!!"
-		hint "Probably you can run the following script to SELINUX"
-		info "curl -s https://raw.githubusercontent.com/indexit-devops/caput/master/vminit.sh | sudo bash"
+		hint "Probably you can run the following script to disable SELINUX"
+		info "  curl -s https://raw.githubusercontent.com/indexit-devops/caput/master/vminit.sh | sudo bash"
 		exit 1
 	fi
+}
+
+CheckFirewall() {
+	
+	case $ELV in 
+		el7)
+			systemctl disable firewalld &>/dev/null
+			systemctl stop firewalld &>/dev/null
+		;;
+		*)  error "OS Version not supported"
+			exit 1
+		;;
+	esac
+	success "Disabled FIREWALL Successfully"
 }
