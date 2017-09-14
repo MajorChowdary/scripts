c4b359b6a0949c0dd0b1a25a3b91cc25f4db4331
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-09-14 12:27:20 +0530

    Updating scripts

--- common-functions.sh
+++ common-functions.sh
@@ -7,7 +7,12 @@ N="\e[0m"
 ## Common Functions
 
 ### Print Functions
-#info() {:;}
+hint() {
+	echo -e "➜ Hint: $1$N"
+}
+info() {
+	echo -e " $1$N"
+}
 #success() {:;}
 error() {
 	echo -e "${R}✗  $1$N"
@@ -27,6 +32,8 @@ CheckSELinux() {
 	STATUS=$(sestatusx | grep 'SELinux status:'| awk '{print $NF}')
 	if [ "$STATUS" != 'disabled' ]; then 
 		error "SELINUX Enabled on the server, Hence cannot proceed. Please Disable it and run again.!!"
+		hint "Probably you can run the following script to SELINUX"
+		info "curl -s https://raw.githubusercontent.com/indexit-devops/caput/master/vminit.sh | sudo bash"
 		exit 1
 	fi
 }
